16ef65fab1d2ef51e2f7d4441766b0c42b70767b
Hernan B. Apaza <chemoran_300@hotmail.com>
2025-11-03 20:44:35 -0500

    add components

--- src/components/ui/aspect-ratio.tsx
+++ src/components/ui/aspect-ratio.tsx
@@ -1,6 +1,4 @@
-'use client'
-
-import * as AspectRatioPrimitive from '@radix-ui/react-aspect-ratio'
+import * as AspectRatioPrimitive from "@radix-ui/react-aspect-ratio"
 
 function AspectRatio({
   ...props
