9e0511d34cef889b8cc3bc5f888f638a78ace003
Hernan B. Apaza <chemoran_300@hotmail.com>
2026-02-03 23:41:57 -0500

    fix(resizable): replace ResizablePrimitive with direct imports for improved clarity and type safety

---
+++ src/types/react-resizable-panels.d.ts
@@ -0,0 +1,11 @@
+declare module 'react-resizable-panels' {
+  import * as React from 'react'
+  export const PanelGroup: React.ComponentType<any>
+  export const Panel: React.ComponentType<any>
+  export const PanelResizeHandle: React.ComponentType<any>
+  export type PanelProps = any
+  export type PanelGroupProps = any
+  export type PanelResizeHandleProps = any
+  const _default: any
+  export default _default
+}
